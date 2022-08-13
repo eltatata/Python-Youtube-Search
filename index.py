453a1879e11a2f31c081d1d9f23f4df78960adf1
teddy171 <77321244+teddy171@users.noreply.github.com>
2022-08-13 19:45:15 +0800

    Update index.py
    feat: Use set to void repeated id

--- index.py
+++ index.py
@@ -8,7 +8,8 @@ text_user = input("cual es la busqueda que deseas realizar: ")
 # covertir el texto normal a texto de busqueda
 converted_text = parse.urlencode({'search_query': text_user})
 HTML = request.urlopen(f"http://www.youtube.com/results?{converted_text}")
-results = re.findall('watch\?v=(.{11})',HTML.read().decode('utf-8'))
+results = set(re.findall('watch\?v=(.{11})',HTML.read().decode('utf-8')))
+results = list(results)
 
 # mostrar el resultado probablemente mas acertado
 print("el resultado de la busqueda es:")
@@ -16,5 +17,5 @@ print(f"https://www.youtube.com/watch?v={results[0]} \n")
 
 # mostrar todos los resultados encontrados
 print("estos son los otros resultados")
-for id in results:
+for id in results[1:]:
   print(f"https://www.youtube.com/watch?v={id}")
